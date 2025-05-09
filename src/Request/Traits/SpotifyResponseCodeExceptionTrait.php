a47e2d1f4b0aef21c9b803c40970604323db330e
Mark Ren <mark.ren77@gmail.com>
2025-05-09 17:10:25 -0500

    added stan at level 3

--- src/Request/Traits/SpotifyResponseCodeExceptionTrait.php
+++ src/Request/Traits/SpotifyResponseCodeExceptionTrait.php
@@ -5,6 +5,7 @@ namespace EXACTSports\Spotify\Request\Traits;
 use EXACTSports\Spotify\Exceptions\SpotifyBadResponseException;
 use EXACTSports\Spotify\Exceptions\SpotifyTokenExpiredException;
 use EXACTSports\Spotify\Exceptions\SpotifyUnauthorizedException;
+use GuzzleHttp\Exception\RequestException;
 
 trait SpotifyResponseCodeExceptionTrait
 {
@@ -15,10 +16,26 @@ trait SpotifyResponseCodeExceptionTrait
      */
     public function handleException(\Exception $exception)
     {
-        match ($exception->getCode()) {
-            401 => throw new SpotifyTokenExpiredException($exception->getMessage()),
-            403 => throw new SpotifyUnauthorizedException($exception->getMessage()),
-            default => throw new SpotifyBadResponseException($exception->getMessage())
+        $statusCode = null;
+        if ($exception instanceof RequestException && $exception->hasResponse()) {
+            $statusCode = $exception->getResponse()->getStatusCode();
+        } else {
+            // Fallback for non-Guzzle exceptions or Guzzle exceptions without a response
+            // or if getCode() is more appropriate for other exception types.
+            $statusCode = $exception->getCode();
+        }
+
+        $message = $exception->getMessage();
+
+        match ($statusCode) {
+            401 => throw new SpotifyTokenExpiredException($message, $statusCode, $exception),
+            403 => throw new SpotifyUnauthorizedException($message, $statusCode, $exception),
+            // It's good practice to include the status code in the default message if it's an HTTP error
+            default => throw new SpotifyBadResponseException(
+                "Spotify API request failed with status code: {$statusCode}. Message: {$message}",
+                is_int($statusCode) ? $statusCode : 0, // Ensure code is an int for the exception constructor
+                $exception
+            )
         };
     }
 }
